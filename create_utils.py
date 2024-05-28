6c2a314687fd2dc3bf996529fe4af00308838b77
248922 <2981497331@qq.com>
2024-05-28 16:26:38 +0800

    #

--- create_utils.py
+++ create_utils.py
@@ -1,8 +1,8 @@
 from langchain.agents import create_openai_functions_agent, AgentExecutor
-from langchain_core.messages import HumanMessage, AIMessage
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import LLMChainExtractor
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.tools import create_retriever_tool
-
 
 def create_agent(input_prompt:str,db,llm):
     prompt = ChatPromptTemplate.from_messages([
@@ -15,9 +15,10 @@ def create_agent(input_prompt:str,db,llm):
     retriever_tool = create_retriever_tool(
         db,
         "retriever_tool",
-        "You should try to use this tool !",
+        "Search for information . For any questions , you must use this tool!",
     )
     tools = [retriever_tool]
     agent = create_openai_functions_agent(llm, tools, prompt)
     agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
     return agent_executor
--- llm.py
+++ llm.py
@@ -12,7 +12,7 @@ from base_prompt import get_base_prompt
 class PDFQuery:
     def __init__(self, openai_api_key = None) -> None:
         self.embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=5000, chunk_overlap=10)
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
         self.llm = ChatOpenAI(api_key=openai_api_key,model="gpt-3.5-turbo", temperature=0)
         self.chain = None
         self.db = None
@@ -33,7 +33,7 @@ class PDFQuery:
         loader = PyPDFLoader(file_path)
         documents = loader.load()
         splitted_documents = self.text_splitter.split_documents(documents)
-        self.db = FAISS.from_documents(splitted_documents, self.embeddings).as_retriever(search_type="similarity", search_kwargs={"k": 10})
+        self.db = FAISS.from_documents(splitted_documents, self.embeddings).as_retriever(search_type="similarity", search_kwargs={"k": 6})
 
     def create_prompt(self, prompt_input: str)-> None:
         self.chain = create_agent(input_prompt=prompt_input, db=self.db, llm=self.llm)
